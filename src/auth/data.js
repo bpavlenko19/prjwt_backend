54b4f1409593ab8d6eb5b2df00934133b01d33db
bpavlenko19 <b.pavlenko@istu.edu.ua>
2024-05-13 21:13:41 +0300

    first commit

---
+++ src/auth/data.js
@@ -0,0 +1,12 @@
+const passwordSecret = "abc";
+
+const fakeUser = {
+    login: "admit",
+    passwordHash: 
+    "C67CF069939B1789EEE32172BA65BDABCD47AE29F18EC948BA8B4440C8E5DD0F",
+};
+
+module.exports = {
+   passwordSecret,
+   fakeUser,
+}
